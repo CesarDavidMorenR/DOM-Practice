07bb675fe017353a9582e47494f186829b3bd06e
cDavid MorenoR <cesardaviddev@gmail.com>
2022-04-01 19:23:46 +0200

    Eventos en JS

--- src/events.js
+++ src/events.js
@@ -1,2 +1,29 @@
-const btn = document.querySelector('#btn');
-btn.onclick = () => alert('Hello, world');
+// Solo con onclick
+
+const btnonclick = document.querySelector('#btnonclick');
+btnonclick.onclick = () => alert('BTN ON CLICK and apart js document');
+
+// Maintaining separtion of concerns and allowing muliple event listeners
+
+const btndos = document.querySelector('#btndos');
+btndos.addEventListener('click', () => alert('Event listener'));
+
+
+function alertFunction() {
+    alert("YAY! YOU DID IT with alert function");
+  }
+
+
+  // estos dos con btn declarado en variable en el documento js
+  const btnalert = document.querySelector('#btnalert');
+  // btnalert.onclick = alertFunction;
+  //btnalert.addEventListener('click', alertFunction);
+/* btnalert.addEventListener('click', function (e) {
+      console.log(e)}); */
+/* btnalert.addEventListener('click', function (e) {
+        console.log(e.target)}); */
+
+  btnalert.addEventListener('click', function (e) {
+      e.target.style.backgroundColor = 'blue'});
+
+   /*    *Note that function (e) is a callback from addEventListener. The e in that function is an object that references the event itself. Within that object you have access to many useful properties and functions such as which mouse button or key was pressed, or information about the event’s target - the DOM node that was clicked.*/
