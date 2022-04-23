6e6943f8b84b4080f3c670917742a6540dce09ca
cesar david moreno <cesardaviddev@gmail.com>
2022-04-23 12:53:09 +0200

    Practicando clases JS import export clases herencia arrays de obketos

---
+++ src/Inmobilaria/inmobilariaOOP.js
@@ -0,0 +1,26 @@
+class Inmobilaria {
+    pisos = [
+        {id: 1,
+        nombre: 'Piso Playa 1',
+        }
+   ];
+
+   constructor() {
+    console.log('Inmobilaria creada');
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+export default Inmobilaria;
---
+++ src/Inmobilaria/main.js
@@ -0,0 +1,6 @@
+import Inmobilaria from './inmobilariaOOP.js';
+
+
+const inmobilaria1 = new Inmobilaria();
+
+console.log(inmobilaria1)
